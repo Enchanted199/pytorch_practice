b08d93e04d36153eee95a653c83dc6c7fc22497b
Enchanted <510594833@qq.com>
2020-07-06 15:18:12 +0800

    123

---
+++ Text_classification.py
@@ -0,0 +1,25 @@
+import torch
+from torchtext import data
+
+SEED = 1234
+
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+TEXT = data.Field(tokenize='spacy')
+LABEL = data.LabelField(dtype=torch.float)
+from torchtext import datasets
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+print(f'Number of training examples: {len(train_data)}')
+print(f'Number of testing examples: {len(test_data)}')
+
+import random
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+print(f'Number of training examples: {len(train_data)}')
+print(f'Number of validation examples: {len(valid_data)}')
+print(f'Number of testing examples: {len(test_data)}')
+TEXT.build_vocab(train_data, max_size=25000, vectors="glove.6B.100d", unk_init=torch.Tensor.normal_)
+LABEL.build_vocab(train_data)
+print(f"Unique tokens in TEXT vocabulary: {len(TEXT.vocab)}")
+print(f"Unique tokens in LABEL vocabulary: {len(LABEL.vocab)}")
